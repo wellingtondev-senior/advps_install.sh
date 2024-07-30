98ca48c614ecd63246499eaa7175511fd48c9dab
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-07-30 02:29:01 -0300

    update

--- 07_ssl.sh
+++ 07_ssl.sh
@@ -44,7 +44,7 @@ fi
 log $YELLOW "Gerando configuração padrão do SSL/TLS..."
 if [ ! -f "/etc/letsencrypt/options-ssl-nginx.conf" ]; then
     sudo mkdir -p /etc/letsencrypt
-    sudo curl -o /etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot-nginx.conf
+    sudo curl -o /etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/options-ssl-nginx.conf
     if [ $? -ne 0 ]; then
         log $RED "Erro ao baixar a configuração padrão do SSL/TLS. Abortando."
         exit 1
