ebee72935af22bbcbd4f4ed61eacd0af78e94f5d
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-07-30 01:40:59 -0300

    update

--- main.sh
+++ main.sh
@@ -25,17 +25,20 @@ log() {
 log $BLUE "Iniciando o script principal para configuração do servidor e projeto..."
 
 # URLs para os scripts individuais
-INSTALL_DEPENDENCIAS="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/01_install_dependencies.sh"
-CONFIGUAR_DOMINIO="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/02_configurar_dominio.sh"
-NODE_PM2_INSTALL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/03_install_nvm_node_pm2.sh"
-SETUP_API="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/04_setup_api.sh"
-SETUP_FRONTEND="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/05_setup_frontend.sh"
-SETUP_POSTGRES="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/06_install_postgresql.sh"
+INSTALL_DEPENDENCIAS_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/01_install_dependencies.sh"
+CONFIGURAR_DOMINIO_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/02_configurar_dominio.sh"
+NODE_PM2_INSTALL_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/03_install_nvm_node_pm2.sh"
+SETUP_API_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/04_setup_api.sh"
+SETUP_FRONTEND_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/05_setup_frontend.sh"
+SETUP_POSTGRES_URL="https://raw.githubusercontent.com/wellingtondev-senior/advps_install.sh/master/06_install_postgresql.sh"
 
 # Caminho para os scripts temporários
 INSTALL_SCRIPT="./01_install_dependencies.sh"
-NGINX_SCRIPT="./02_configure_nginx.sh"
-PROJECT_SCRIPT="./03_setup_project.sh"
+CONFIGURAR_DOMINIO_SCRIPT="./02_configurar_dominio.sh"
+NODE_PM2_INSTALL_SCRIPT="./03_install_nvm_node_pm2.sh"
+SETUP_API_SCRIPT="./04_setup_api.sh"
+SETUP_FRONTEND_SCRIPT="./05_setup_frontend.sh"
+SETUP_POSTGRES_SCRIPT="./06_install_postgresql.sh"
 
 # Função para baixar e verificar os scripts
 download_script() {
@@ -51,9 +54,12 @@ download_script() {
 }
 
 # Baixar e executar os scripts individuais
-download_script "$INSTALL_SCRIPT_URL" "$INSTALL_SCRIPT"
-download_script "$NGINX_SCRIPT_URL" "$NGINX_SCRIPT"
-download_script "$PROJECT_SCRIPT_URL" "$PROJECT_SCRIPT"
+download_script "$INSTALL_DEPENDENCIAS_URL" "$INSTALL_SCRIPT"
+download_script "$CONFIGURAR_DOMINIO_URL" "$CONFIGURAR_DOMINIO_SCRIPT"
+download_script "$NODE_PM2_INSTALL_URL" "$NODE_PM2_INSTALL_SCRIPT"
+download_script "$SETUP_API_URL" "$SETUP_API_SCRIPT"
+download_script "$SETUP_FRONTEND_URL" "$SETUP_FRONTEND_SCRIPT"
+download_script "$SETUP_POSTGRES_URL" "$SETUP_POSTGRES_SCRIPT"
 
 # Executar o script de instalação das dependências
 log $YELLOW "Executando o script de instalação das dependências..."
@@ -63,19 +69,43 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Executar o script de configuração do NGINX
-log $YELLOW "Executando o script de configuração do NGINX..."
-bash "$NGINX_SCRIPT"
+# Executar o script de configuração do domínio (NGINX)
+log $YELLOW "Executando o script de configuração do domínio (NGINX)..."
+bash "$CONFIGURAR_DOMINIO_SCRIPT"
 if [ $? -ne 0 ]; then
-    log $RED "Erro ao executar o script de configuração do NGINX. Abortando."
+    log $RED "Erro ao executar o script de configuração do domínio (NGINX). Abortando."
     exit 1
 fi
 
-# Executar o script de configuração do projeto
-log $YELLOW "Executando o script de configuração do projeto..."
-bash "$PROJECT_SCRIPT"
+# Executar o script de instalação do Node.js e PM2
+log $YELLOW "Executando o script de instalação do Node.js e PM2..."
+bash "$NODE_PM2_INSTALL_SCRIPT"
 if [ $? -ne 0 ]; then
-    log $RED "Erro ao executar o script de configuração do projeto. Abortando."
+    log $RED "Erro ao executar o script de instalação do Node.js e PM2. Abortando."
+    exit 1
+fi
+
+# Executar o script de configuração da API
+log $YELLOW "Executando o script de configuração da API..."
+bash "$SETUP_API_SCRIPT"
+if [ $? -ne 0 ]; then
+    log $RED "Erro ao executar o script de configuração da API. Abortando."
+    exit 1
+fi
+
+# Executar o script de configuração do Frontend
+log $YELLOW "Executando o script de configuração do Frontend..."
+bash "$SETUP_FRONTEND_SCRIPT"
+if [ $? -ne 0 ]; then
+    log $RED "Erro ao executar o script de configuração do Frontend. Abortando."
+    exit 1
+fi
+
+# Executar o script de instalação do PostgreSQL
+log $YELLOW "Executando o script de instalação do PostgreSQL..."
+bash "$SETUP_POSTGRES_SCRIPT"
+if [ $? -ne 0 ]; then
+    log $RED "Erro ao executar o script de instalação do PostgreSQL. Abortando."
     exit 1
 fi
 
