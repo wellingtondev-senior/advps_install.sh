08fb1732472a6b18bc10998880c865f839391828
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-07-31 22:25:39 -0300

    update

--- dashboard/create_dominio.sh
+++ dashboard/create_dominio.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # Cores para log
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -9,12 +7,19 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # Sem cor
 
+# Função de log para imprimir mensagens coloridas
+log() {
+  local color="$1"
+  shift
+  echo -e "${color}$@${NC}"
+}
+
 # Verifica se as variáveis de ambiente PORT e DOMAIN estão definidas
-if [ -z "$PORT" ] || [ -z "$DOMINIO" ]; then
+if [ -z "$PORT" ] || [ -z "$DOMAIN" ]; then
   echo "As variáveis de ambiente PORT e DOMAIN devem ser definidas."
-  echo "Exemplo: PORT=80 DOMINIO=example.com ./script.sh"
+  echo "Exemplo: PORT=80 DOMAIN=example.com ./script.sh"
   exit 1
 fi
 
-log $GREEN "A PORTA QUE VC QUE SETAR $PORT PARA DOMINIO $DOMINIO"
-
+# Log da porta e domínio definidos
+log $GREEN "A PORTA QUE VOCÊ DEFINIU É $PORT PARA O DOMÍNIO $DOMAIN"
