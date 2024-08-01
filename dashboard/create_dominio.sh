870f3f740df35717a94356d8f3cf5495303b9c15
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-07-31 22:19:30 -0300

    update

--- dashboard/create_dominio.sh
+++ dashboard/create_dominio.sh
@@ -10,11 +10,11 @@ BLUE='\033[0;34m'
 NC='\033[0m' # Sem cor
 
 # Verifica se as variáveis de ambiente PORT e DOMAIN estão definidas
-if [ -z "$PORT" ] || [ -z "$DOMAIN" ]; then
+if [ -z "$PORT" ] || [ -z "$DOMINIO" ]; then
   echo "As variáveis de ambiente PORT e DOMAIN devem ser definidas."
   echo "Exemplo: PORT=80 DOMAIN=example.com ./script.sh"
   exit 1
 fi
 
-log $GREEN "A PORTA QUE VC QUE SETAR $PORT PARA DOMINIO $DMINIO"
+log $GREEN "A PORTA QUE VC QUE SETAR $PORT PARA DOMINIO $DOMINIO"
 
