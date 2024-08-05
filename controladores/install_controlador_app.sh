ef57d0084599ade66fc91aae1e92152a4e686bf2
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-08-04 21:20:42 -0300

    update

---
+++ controladores/install_controlador_app.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Cores para log
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # Sem cor
+IP=$(curl -s https://ipinfo.io/ip)
+
+# Exibe o IP (opcional)
+
+# Função de log para imprimir mensagens com timestamps e cores
+log() {
+    local color=$1
+    shift
+    echo -e "${color}[$(date '+%Y-%m-%d %H:%M:%S')] $@${NC}"
+}
+
+log $BLUE "Iniciando a configuração do BACKEND..."
+
+# Configuração do diretório do projeto e execução do Docker
+PROJECT_DIR="/home/devcloud/controlapp.devcloud.top"
+REPO_URL="git@github.com:wellingtondev-senior/controladora.app.devcloud.top.git"
+
+# Criar o diretório do projeto e clonar o repositório
+log $YELLOW "Criando o diretório do projeto e clonando o repositório..."
+sudo mkdir -p $PROJECT_DIR
+sudo git clone $REPO_URL $PROJECT_DIR
+
+# Garantir que o diretório do projeto pertença ao usuário atual
+sudo chown -R $USER:$USER $PROJECT_DIR
+
+# Entrar no diretório do projeto
+log $YELLOW "Entrando no diretório do projeto..."
+cd $PROJECT_DIR
+
+# Verificar se o arquivo package.json existe
+if [ ! -f "$PROJECT_DIR/package.json" ]; then
+    log $RED "Erro: O arquivo package.json não foi encontrado no diretório $PROJECT_DIR."
+    exit 1
+fi
+
+# Instalar dependências e construir o projeto
+log $YELLOW "Instalando dependências do projeto..."
+npm install
+
+log $YELLOW "Construindo o projeto..."
+npm run build
+
+
+
+# Verificar se a porta está disponível
+
+
+# Atualizar o arquivo de configuração para usar a porta dinâmica
+# Supondo que você tenha um arquivo .env para definir variáveis de ambiente
+
+log $YELLOW "Iniciando o projeto com PM2..."
+# Iniciar o projeto com PM2 e definir o nome da aplicação como "api"
+pm2 start npm --name controlapp_devcloud -- run start:prod
+
+# Instalar e configurar o firewall para permitir a porta dinâmica
+log $YELLOW "Instalando e configurando o firewall..."
+
+yes | sudo ufw enable
+# Permitir a porta dinâmica no firewall
+sudo ufw allow 58553/tcp
+
+# Habilitar o firewall, se ainda não estiver habilitado
+
+# Mostrar o status do firewall
+sudo ufw status
+
+log $GREEN "Configuração concluída com sucesso!"
+
+# Finalizar
+ log $GREEN "###########################################################"
+ log $GREEN "Aplicação Contralador App:"
+ log $GREEN "Acesse ao Microserviço Contralador App em: http://$IP:58553"
+ log $GREEN "###########################################################"
